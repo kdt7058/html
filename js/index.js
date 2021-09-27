09d94be9d2390a4eab69351bfc8f6e287e06103a
omtapse <omtapse01@gmail.com>
2021-09-27 11:37:57 +0530

    27/09 commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// console.log("Hi Everyone!")
+// document.write("JS is New Lang")
+
+// var num1 = 21
+// var num2 = 21
+// // console.log(num1+num2)
+
+// // console.log("This is String");
+
+// var obj = { 
+//     name:"Krishna",
+//     Age:22,
+//     Address : "Pune",
+// }
+// //console.log(obj);
+
+// //boolean
+// var a =true
+// console.log(a)
+
+// //undefined
+// var b;
+// console.log(b)
+
+// //Premitive Data type: undifined, null, number,String,boolean,symbol
+// //Referance Data type: Array, objects
+
+// var arr = [1,2,34,5];
+// console.log(arr);
+// // arr.forEach(function(element){
+// //     console.log(element)
+// // })
+
+// //Array shift and unshift
+// // arr.shift(5)
+// // console.log(arr)
+
+// // arr.unshift(115)
+// // console.log(arr)
+
+// //String methode
+// var str = "Harry Code"
+// // console.log(str.indexOf("o")) 
+// // console.log(str.slice(1,3))
+// // console.log(str.replace("Harry","Krishna"))
+
+// //Date
+// let myDate = new Date()
+// // console.log(myDate);
+// console.log("Date:"+ myDate.getDate());
+// console.log(myDate.getFullYear());
+
+//DOM Manupulation
+
+// var element = document.getElementById("click");
+// console.log(element);
+const element = document.getElementById("firstContainer");
+console.log(element);
+// element[0].style.background = "yellow";
+element.classList.add("bg-primary");    
